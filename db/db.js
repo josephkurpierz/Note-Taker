8f6e8694224fd5eb1b81a6e667c8dd6a9a732e3d
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-07-16 16:30:58 -0500

    delete notes

--- db/db.js
+++ db/db.js
@@ -3,14 +3,16 @@ const dbJson = require("./db.json");
 const util = require('util');
 const fs = require("fs");
 const path = require("path");
-const { v4: uuidv4 } = require('uuid');//use uuidv4() to create random id
+const { v4: uuidv4 } = require('uuid');
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
+// read json objects from db.json
 function read() {
   return readFileAsync("db/db.json", "utf8");
 }
 
+//write json object to db.json
 function write(note) {
   return writeFileAsync("db/db.json", JSON.stringify(note));
 }
@@ -24,18 +26,26 @@ function getNotes() {
 
 function createNote(info){
   getNotes();
-  console.log(getNotes());
   const {title, text} = info;
   const newNote = {title, text, id:uuidv4()};
-  console.log(newNote);
-  // noteArr.push(note);
   return getNotes()
     .then((notes)=>[...notes,newNote])
     .then((updatedNotes)=>write(updatedNotes))
     .then(() => newNote);
 };
 
+// function deleteNote(id){
+//   getNotes()
+//     .then((data) => JSON.parse(data))
+//     .then((json) => {
+//       const result = json.filter((id)=>id!==)
+//     })
+
+
+// }
+
 module.exports = {
   createNote,
-  getNotes
+  getNotes,
+  write
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,27 +2,29 @@ const router = require("express").Router();
 const dbJson = require("../db/db.json");
 const fs = require("fs");
 const path = require("path");
-const {createNote, getNotes} = require("../db/db");
+const { createNote, getNotes, write } = require("../db/db.js");
 
 router.get("/notes", (req, res) => {
   getNotes()
-    .then((notes)=> {
+    .then((notes) => {
       return res.json(notes);
     })
-    .catch((err)=>
-      res.status(500).json(err)
-    )
+    .catch((err) => res.status(500).json(err));
 });
 
 router.post("/notes", (req, res) => {
-  console.log("post api note");
   const note = createNote(req.body);
   res.json(note);
 });
 
-// router.delete("/notes/:id", (req,res) => {
-//   console.log("req params", req.params.id);
-//   dbJson = dbJson.filter(({ id }) => id !== req.params.id);
-// })
+router.delete("/notes/:id", (req, res) => {
+  let deleteId = req.params.id;
+  getNotes()
+    .then((notes) => {
+      const results = notes.filter((notes) => notes.id !== deleteId);
+      write(results);
+    });
+
+});
 
 module.exports = router;
