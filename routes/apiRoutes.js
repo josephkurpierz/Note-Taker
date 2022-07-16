97475efac74796bd595dfc93c28fddabb9c04038
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-07-16 13:30:58 -0500

    Chris helped

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,37 +2,27 @@ const router = require("express").Router();
 const dbJson = require("../db/db.json");
 const fs = require("fs");
 const path = require("path");
-const { v4: uuidv4 } = require('uuid');//use uuidv4() to create random id
-
-
-function createNote(info,dbJson){
-  const note = info;
-  console.log("  info =", info, "  dbJson =", dbJson);
-  dbJson.push(note);
-  fs.writeFileSync(path.join(__dirname,"../db/db.json"),JSON.stringify    ({dbJson},null, 2));
-  return info;
-};
+const {createNote, getNotes} = require("../db/db");
 
 router.get("/notes", (req, res) => {
-  console.log("get api notes")
-  let results = dbJson;
-  if (results) {
-    res.json(results);
-  } else {
-    res.send(404);
-  }
+  getNotes()
+    .then((notes)=> {
+      return res.json(notes);
+    })
+    .catch((err)=>
+      res.status(500).json(err)
+    )
 });
 
 router.post("/notes", (req, res) => {
   console.log("post api note");
-  req.body.id = uuidv4();
-  const note = createNote(req.body,dbJson);
+  const note = createNote(req.body);
   res.json(note);
 });
 
-router.delete("/notes/:id", (req,res) => {
-  console.log("req params", req.params.id);
-  dbJson = dbJson.filter(({ id }) => id !== req.params.id);
-})
+// router.delete("/notes/:id", (req,res) => {
+//   console.log("req params", req.params.id);
+//   dbJson = dbJson.filter(({ id }) => id !== req.params.id);
+// })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { notes } = require("./db/db.json");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
-const { v4: uuidv4 } = require('uuid');//use uuidv4() to create random id
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
